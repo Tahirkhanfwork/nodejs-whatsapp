efe2e68166816985ec7657237525da656696d9d4
Tahirkhanfwork <tahirkhanfwork@gmail.com>
2024-10-03 11:06:36 +0530

    Update WhatsappMessage.js

--- models/WhatsappMessage.js
+++ models/WhatsappMessage.js
@@ -1,26 +1,23 @@
 const mongoose = require("mongoose");
 
+const MessageSchema = new mongoose.Schema({
+  message_id: { type: String },
+  message_text_body: { type: String },
+  message_type: { type: String },
+  timestamp: { type: String },
+  button_id: { type: String },
+  button_title: { type: String },
+}, { _id: false }); // Disabling _id for subdocuments to avoid duplication of IDs
+
 const WhatsappMessageSchema = new mongoose.Schema({
   messaging_product: { type: String, required: true },
   display_phone_number: { type: String, required: true },
   phone_number_id: { type: String, required: true },
-  status: { type: String },
-  timestamp: { type: String },
-  recipient_id: { type: String },
-  conversation_id: { type: String },
-  conversation_expiration_timestamp: { type: String },
-  conversation_origin_type: { type: String },
-  pricing_billable: { type: Boolean },
-  pricing_model: { type: String },
-  pricing_category: { type: String },
   contact_name: { type: String },
   contact_wa_id: { type: String },
-  message_from: { type: String },
-  message_id: { type: String },
-  message_text_body: { type: String },
-  message_type: { type: String },
-  button_id: { type: String },
-  button_title: { type: String },
+  recipient_id: { type: String },
+  conversation_id: { type: String },
+  messages: [MessageSchema] // Array to store all messages exchanged with the user
 }, { timestamps: true });
 
 const WhatsappMessage = mongoose.model("WhatsappMessage", WhatsappMessageSchema);
